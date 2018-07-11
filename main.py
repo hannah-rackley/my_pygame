2ad786fbabad8b59ea5bf1811df39fbc38e02986
Hannah <hrglasser94@gmail.com>
2018-07-10 21:03:27 -0400

    Refactored so that screen size, box number, and box size, can be adjusted easily.

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ pygame.init()
 BLUE = (159, 232, 252)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+ROCK = (104, 115, 132)
 
 #Setup our screen size.
-size = (600, 600)
+box_length = 85
+margin = 2
+box_number = 6
+
+width = box_number * (box_length + margin)
+height = box_number * (box_length + margin)
+
+size = (width, height)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Save the penguins!")
-
-width = 114
-height = 114
-margin = 5
 
 class Board(object):
 
@@ -21,19 +25,19 @@ class Board(object):
         self.grid = []
     
     def create_grid_array(self):
-        for row in range(5):
+        for row in range(box_number):
             self.grid.append([])
-            for column in range(5):
+            for column in range(box_number):
                 self.grid[row].append(0)
         return self.grid
 
     def draw_board(self):
         grid = self.create_grid_array()
         screen.fill(BLACK)
-        for row in range(5):
-            for column in range(5):
+        for row in range(box_number):
+            for column in range(box_number):
                 color = BLUE
-                pygame.draw.rect(screen, color, [(margin + width) * column + margin, (margin + height) * row + margin, width, height])
+                pygame.draw.rect(screen, color, [(margin + box_length) * column + margin, (margin + box_length) * row + margin, box_length, box_length])
         return grid
 
 class Player(pygame.sprite.Sprite):
@@ -55,20 +59,20 @@ class Player(pygame.sprite.Sprite):
     def change_move_speed(self, pressed):
         if self.move_horizontal == 0 and self.move_vertical == 0:
             if pressed[pygame.K_RIGHT]:
-                self.move_horizontal = 3
+                self.move_horizontal = 10
             elif pressed[pygame.K_LEFT]:
-                self.move_horizontal = -3
+                self.move_horizontal = -10
             elif pressed[pygame.K_UP]:
-                self.move_vertical = -3
+                self.move_vertical = -10
             elif pressed[pygame.K_DOWN]:
-                self.move_vertical = 3
+                self.move_vertical = 10
 
     def update(self):
-        if self.rect.right + self.move_horizontal >= 600:
+        if self.rect.right + self.move_horizontal >= width:
             self.move_horizontal = 0
         elif self.rect.left + self.move_horizontal <= 0:
             self.move_horizontal = 0
-        if self.rect.bottom + self.move_vertical >= 600:
+        if self.rect.bottom + self.move_vertical >= height:
             self.move_vertical = 0
         elif self.rect.top + self.move_vertical <= 0:
             self.move_vertical = 0
@@ -80,15 +84,29 @@ class Hole(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super(Hole, self).__init__()
     
-        self.image = pygame.Surface([114, 114])
+        self.image = pygame.Surface([box_length, box_length])
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
 
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = (box_length + margin) * x + margin
+        self.rect.y = (box_length + margin) * y + margin
 
     def check_hole_collision(self):
        pygame.sprite.spritecollide(self, all_sprites_list, True)
+
+# class Rock(pygame.sprite.Sprite):
+#     def __init__(self, x, y):
+#         super(Rock, self).__init__()
+    
+#         self.image = pygame.Surface([box_length, box_length])
+#         self.image.fill(BLACK)
+#         self.rect = self.image.get_rect()
+
+#         self.rect.x = (box_length + margin) * x + margin
+#         self.rect.y = (box_length + margin) * y + margin
+
+#     def check_hole_collision(self):
+#        pygame.sprite.spritecollide(self, all_sprites_list, True)
             
     
 #Setup board      
@@ -100,7 +118,7 @@ all_sprites_list = pygame.sprite.Group()
 all_sprites_list.add(player)
 
 #Create hole
-hole = Hole(124, 481)
+hole = Hole(0, 2)
 hole_list = pygame.sprite.Group()
 hole_list.add(hole)
 
